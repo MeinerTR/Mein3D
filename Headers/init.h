36ddb104312aacbd5877f57bc0445a8aa08c82dd
MeinerTR <87811811+MeinerTR@users.noreply.github.com>
2022-03-16 19:02:06 +0300

    Update init.h

--- Headers/init.h
+++ Headers/init.h
@@ -1,10 +1,12 @@
-void Reshape(GLFWwindow* _Window, const int _Width, const int _Height) {glViewport(0, 0, _Width, _Height);}
+void Reshape(GLFWwindow* _Window, const int _Width, int _Height) {glViewport(0, 0, _Width, _Height);}
 
 void HandleKeyboard() {
 	if (glfwGetKey(Window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(Window, true);
 
     float Camera__ = static_cast<float>(2.5 * DeltaTime);
+    if (glfwGetKey(Window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {Camera__ += 0.3;}
+    
     if (glfwGetKey(Window, GLFW_KEY_W) == GLFW_PRESS)
         _Camera += Camera__ * Camera_;
     if (glfwGetKey(Window, GLFW_KEY_S) == GLFW_PRESS)
@@ -21,7 +23,7 @@ void HandleMouseScrool(GLFWwindow* window, const double OffsetX, const double Of
     if (Fov > 45.0f) {Fov = 45.0f;}
 }
 
-void HandleMouse(GLFWwindow *_Window, const double MouseX, const double MouseY) {
+void HandleMouse(GLFWwindow *_Window,  double MouseX,  double MouseY) {
 	float PosX = static_cast<float>(MouseX);
     float PosY = static_cast<float>(MouseY);
 
@@ -30,14 +32,15 @@ void HandleMouse(GLFWwindow *_Window, const double MouseX, const double MouseY) 
         FirstCalled = false;      }
 
     float OffsetX = PosX - LastX;
-    float OffsetY = LastX - PosY;
+    float OffsetY = LastY - PosY;
     LastX = PosX; LastY = PosY;
     float Sensitivity = 0.1f; // change this value to your liking
     OffsetX *= Sensitivity;
     OffsetY *= Sensitivity;
 	Yaw += OffsetX; Pitch += OffsetY;
     if (Pitch > 89.0f) {Pitch = 89.0f;}
-    if (Pitch < -89.0f) {Pitch = -89.0f;} glm::vec3 Front;
+    if (Pitch < -89.0f) {Pitch = -89.0f;} 
+    glm::vec3 Front;
     Front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
     Front.y = sin(glm::radians(Pitch));
     Front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
@@ -45,21 +48,29 @@ void HandleMouse(GLFWwindow *_Window, const double MouseX, const double MouseY) 
 }
 
 void InitMein3D() {
-	if ((CWD = getcwd(NULL, 0)) == NULL) {Error("Failed to get current directory!", 0);}
-	try { Width = std::stoi(INI(Merged(CWD, "/settings.ini"), "Window", 0));
-	} catch (const std::invalid_argument & e) {
-		std::cout << CWD << "/settings.ini -> Width -> Invalid_Argument!" << std::endl;
-		Width = 1280; }
-	try { Height = std::stoi(INI(Merged(CWD, "/settings.ini"), "Window", 1));
-	} catch (const std::invalid_argument & e) {
-		std::cout << CWD << "/settings.ini -> Height -> Invalid_Argument!" << std::endl;
-		Height = 768; }
+	// try { Width = std::stoi(INI(Merged(CWD, "/settings.ini"), "Window", 0));
+	// } catch (const std::invalid_argument & e) {
+	// 	std::cout << CWD << "/settings.ini -> Width -> Invalid_Argument!" << std::endl;
+	// 	Width = 1280; }
+	// try { Height = std::stoi(INI(Merged(CWD, "/settings.ini"), "Window", 1));
+	// } catch (const std::invalid_argument & e) {
+	// 	std::cout << CWD << "/settings.ini -> Height -> Invalid_Argument!" << std::endl;
+	// 	Height = 768; }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	#ifdef __APPLE__ 
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	#endif
+
 	Window = glfwCreateWindow(Width, Height, "Mein3D", NULL, NULL);
 	glfwMakeContextCurrent(Window); if (!Window) {Error("Unable to create GLFW3 window!", 0);}
 	glfwSetWindowSizeLimits(Window, 640, 480, GLFW_DONT_CARE, GLFW_DONT_CARE);
 }
 
 void InitOpenGL() {
+	if ((CWD = getcwd(NULL, 0)) == NULL) {Error("Failed to get current directory!", 0);}
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {Error("Unable to init GLAD!", 1);}
 	glViewport(0, 0, Width, Height); glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClearDepth(1.0f); glEnable(GL_DEPTH_TEST); glDepthFunc(GL_LEQUAL);
