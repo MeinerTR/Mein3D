ab1f554e71781b1d8eb5926b718876a31221df3d
MeinerTR <87811811+MeinerTR@users.noreply.github.com>
2022-03-16 15:34:56 +0300

    Update display.h

--- Headers/display.h
+++ Headers/display.h
@@ -1,9 +1,3 @@
 void HandleDisplay() {
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-   glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
-
-   glLoadIdentity();
-   glTranslatef(_Camera.x, _Camera.y, _Camera.z);
-
-	glfwSwapBuffers(Window); 
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); glfwSwapBuffers(Window);
 }
