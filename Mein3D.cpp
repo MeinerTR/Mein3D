fd9c44300a7dd18d2fba2ee0bccadb8ee673c126
MeinerTR <87811811+MeinerTR@users.noreply.github.com>
2022-03-16 19:01:05 +0300

    Update Mein3D.cpp

--- Mein3D.cpp
+++ Mein3D.cpp
@@ -19,20 +19,123 @@ GLFWwindow *Window; void Error(const char *Error_Message, int Way) {
 
 #include "Headers/input.h"
 #include "Headers/init.h"
-#include "Headers/display.h"
+#include "Headers/shader.h"
 
 int main(void) {
     if (!glfwInit()) {Error("Unable to init GLFW3!", 0);}
     InitMein3D(); InitOpenGL();
+    _Shader Shader(Merged(getcwd(NULL, 0), "/Shaders/camera.vs").c_str(),
+                   Merged(getcwd(NULL, 0), "/Shaders/camera.fs").c_str());
     while (!glfwWindowShouldClose(Window)) {
         HandleKeyboard();
+
+        float CurrentFrame = 
+        static_cast<float>(glfwGetTime());
+        DeltaTime = CurrentFrame - LastFrame;
+        LastFrame = CurrentFrame;
 
         glfwSetCursorPosCallback(Window, HandleMouse);
         glfwSetScrollCallback(Window, HandleMouseScrool);
         glfwSetFramebufferSizeCallback(Window, Reshape);
 
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+
+        float vertices[] = {
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+        };
+        // world space positions of our cubes
+        glm::vec3 cubePositions[] = {
+            glm::vec3( 0.0f,  0.0f,  0.0f),
+            glm::vec3( 2.0f,  5.0f, -15.0f),
+            glm::vec3(-1.5f, -2.2f, -2.5f),
+            glm::vec3(-3.8f, -2.0f, -12.3f),
+            glm::vec3( 2.4f, -0.4f, -3.5f),
+            glm::vec3(-1.7f,  3.0f, -7.5f),
+            glm::vec3( 1.3f, -2.0f, -2.5f),
+            glm::vec3( 1.5f,  2.0f, -2.5f),
+            glm::vec3( 1.5f,  0.2f, -1.5f),
+            glm::vec3(-1.3f,  1.0f, -1.5f)
+        };
+
+        unsigned int VBO, VAO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        // position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        Shader.use();
+
+        // pass projection matrix to shader (note that in this case it could change every frame)
+        glm::mat4 Projection = glm::perspective(glm::radians(Fov), (float) Width / (float) Height, 0.1f, 100.0f);
+        Shader.setMat4("Projection", Projection);
+
+        // camera/view transformation
+        glm::mat4 View = glm::lookAt(_Camera, _Camera + Camera_, Cam_era);
+        Shader.setMat4("View", View);
+
+        glBindVertexArray(VAO);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 Model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+            Model = glm::translate(Model, cubePositions[i]);
+            float Angle = 20.0f * i;
+            Model = glm::rotate(Model, glm::radians(Angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            Shader.setMat4("Model", Model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
         glfwPollEvents();
-        HandleDisplay();
+        glfwSwapBuffers(Window);
         
     } glfwTerminate(); return EXIT_SUCCESS;
 } 
